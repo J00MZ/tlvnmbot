0868b2c147ce63d7b90d3bde8bfa6845ea4c1b68
Joe Tavin <jtavin@gmail.com>
2023-07-03 10:14:48 +0300

    Initial Code Commit Add .gitignore and .pre-commit-config.yaml

---
+++ app.py
@@ -0,0 +1,76 @@
+import os
+import sqlite3
+import time
+from sqlite3 import Error
+
+import requests
+from bs4 import BeautifulSoup
+
+TOKEN = os.environ.get("BOT_TOKEN")
+CHAT_ID = os.environ.get("CHAT_ID")
+
+
+def create_connection():
+    conn = None
+    db_file = os.path(os.environ.get("PWD") + "price.db")
+    try:
+        conn = sqlite3.connect(db_file)
+        if conn:
+            print(sqlite3.version)
+    except Error as e:
+        print(e)
+
+    if conn:
+        return conn
+    else:
+        return None
+
+
+def create_table(conn):
+    try:
+        query = """CREATE TABLE IF NOT EXISTS PriceData (
+                    time text NOT NULL,
+                    price text NOT NULL
+                );"""
+        conn.execute(query)
+    except Error as e:
+        print(e)
+
+
+def send_message(text: str, chat_id: str, token: str):
+    base_url = (
+        f"https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={text}"
+    )
+    requests.get(base_url)
+
+
+def parse_price():
+    response = requests.get("https://fastlane.co.il/")
+    soup = BeautifulSoup(response.text, "html.parser")
+    price = soup.find("span", {"id": "lblPrice"}).text
+    print("bs found price: %s", price)
+    return price
+
+
+def main():
+    conn = create_connection()
+    create_table(conn)
+
+    last_price = None
+    while True:
+        try:
+            current_price = parse_price()
+            if current_price != last_price:
+                send_message(f"New price: {current_price}", CHAT_ID, TOKEN)
+                last_price = current_price
+
+                # insert last_price into the database
+                with conn:
+                    query = """INSERT INTO PriceData(time, price) VALUES(?, ?);"""
+                    conn.execute(
+                        query, (time.strftime("%Y-%m-%d %H:%M:%S"), last_price)
+                    )
+
+        except Exception as e:
+            send_message(f"Error: {e}", "YourChatId", "YourBotToken")
+        time.sleep(300)  # wait for 5 minutes
