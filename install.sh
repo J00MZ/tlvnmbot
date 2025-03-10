8826b4e6154e081b0f4284822631f9522b781018
Joe Tavin <jtavin@gmail.com>
2025-03-10 16:12:02 +0200

    fix some

--- install.sh
+++ install.sh
@@ -7,7 +7,9 @@ dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo -y
 #gh auth login
 #gh repo clone J00MZ/tlvnmbot
 cd tlvnmbot
-python3 -p pip3 install poetry
+python -m venv .venv
+source .venv/bin/activate
+python -m pip install poetry
 poetry install
 mkdir logs
 TG_TOKEN=$TG_TOKEN nohup poetry run python3 app.py >> logs/run.log 2>&1 &
